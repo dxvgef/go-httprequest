b7b78ef084c7b7820532aac3a961a33546cf9358
dxvgef <dxvgef@outlook.com>
2021-08-27 10:12:32 +0800

    清理无用代码

--- util.go
+++ util.go
@@ -9,13 +9,6 @@ func BytesToStr(value []byte) string {
 	return *(*string)(unsafe.Pointer(&value))
 }
 
-// 字符串转[]byte
-func StrToBytes(s string) []byte {
-	x := (*[2]uintptr)(unsafe.Pointer(&s))
-	h := [3]uintptr{x[0], x[1], x[1]}
-	return *(*[]byte)(unsafe.Pointer(&h))
-}
-
 // 在[]int中判断是否存在指定的值
 func inIntSlice(status int, retryStatus []int) bool {
 	for k := range retryStatus {
